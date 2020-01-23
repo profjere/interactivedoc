3b2762c64fb73fc8f034700c4c3f8221f1de4ff7
Jeremy Sarachan <jsarachan@sjfc.edu>
2020-01-23 14:13:57 -0500

    Add files via upload

---
+++ SVG_Shapes_Example/blocks.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $("#red").click(function(){
+    $("#answer").html("<h1>Red Rectangle.</h1>")
+  });
+    $("#brick").click(function(){
+    $("#answer").html("<h1>Brick Colored Rectangle.</h1>")
+  });
+    $("#polygon").click(function(){
+    $("#answer").html("<h1>Hexagon.</h1>")
+  });
+});
